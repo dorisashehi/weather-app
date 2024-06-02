2a98a25d6fa2d0f96ffb8927acda47de2ae7e4a1
Dorisa Shehi <doris.shehi2@gmail.com>
2024-06-02 15:16:21 +0200

    Removed unused code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './assets/styles/main.scss'
 import './assets/images/spinner.gif'
 import './assets/images/partly-cloudy.jpg'
-import { sidebarModule } from './modules/sidebar';
-import {contentModule } from "./modules/content";
 import { getLocationData } from "./modules/app";
 
 getLocationData(); //GET DEFAULT WEATHER LOCATION
