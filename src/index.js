fede4cdfd165bf3c971d04dac1d571c5213c5154
Dorisa Shehi <doris.shehi2@gmail.com>
2024-06-03 19:38:41 +0200

    Rebuilded

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './assets/styles/main.scss'
-import './assets/images/spinner.gif'
-import './assets/images/partly-cloudy.jpg'
-import { getLocationData } from "./modules/app";
-
-
-getLocationData(); //GET DEFAULT WEATHER LOCATION
--- src/modules/spinner.js
+++
@@ -1,8 +0,0 @@
-import { spinner } from  '../assets/images/spinner.gif';
-const showSpinner = (value) => {
-
-    const spinnerContainer = document.querySelector('.loading-spinner');
-    (value) ? spinnerContainer.style.display = 'none' : spinnerContainer.style.display = 'block';
-}
-
-export { showSpinner }
